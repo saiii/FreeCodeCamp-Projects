fe4260cff8853641f637f845c242b7d2f9cb1205
maxjunerd <maxjunerd@gmail.com>
2019-11-11 10:41:48 +0700

    Add comment

--- JavaScript Algorithms and Data Structures/Palindrome Checker.js
+++ JavaScript Algorithms and Data Structures/Palindrome Checker.js
@@ -1,25 +1,32 @@
+// Check if the given parameter is palindrome
+
 function palindrome(str) {
-    // Good luck!
+    // Delete space, special character and convert every character to lowwer case
     str = str.replace(/\W|_|\s/g,"").toLowerCase();
 
     let reverse = [];
     
+    // Loop from the end of str parameter to the start and append to reverse variable
     for (let i = str.length-1; i >= 0; i--) {
         reverse.push(str[i]);
     }
+
+    // Use join method to convert array to string
     reverse = reverse.join("");
+
+    // Check if str parameter and reverse variable (str after reverse) is the same
     return str === reverse;
   }
   
-palindrome("eye")//?
-palindrome("_eye")//?
-palindrome("race car")//?
-palindrome("not a palindrome")//?
-palindrome("A man, a plan, a canal. Panama")//?
-palindrome("never odd or even")//?
-palindrome("nope")//?
-palindrome("almostomla")//?
-palindrome("My age is 0, 0 si ega ym.")//?
-palindrome("1 eye for of 1 eye.")//?
-palindrome("0_0 (: /-\ :) 0-0")//?
-palindrome("five|\_/|four")//?
+palindrome("eye")
+palindrome("_eye")
+palindrome("race car")
+palindrome("not a palindrome")
+palindrome("A man, a plan, a canal. Panama")
+palindrome("never odd or even")
+palindrome("nope")
+palindrome("almostomla")
+palindrome("My age is 0, 0 si ega ym.")
+palindrome("1 eye for of 1 eye.")
+palindrome("0_0 (: /-\ :) 0-0")
+palindrome("five|\_/|four")
