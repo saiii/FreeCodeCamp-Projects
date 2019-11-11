7276a0936be02570a5076c94f369ab4dde91e0fa
maxjunerd <maxjunerd@gmail.com>
2019-11-11 10:46:26 +0700

    Test more parameters

--- JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
@@ -22,4 +22,29 @@ function convertToRoman(num) {
     return romanized;
    }
 
-convertToRoman(36);
+   convertToRoman(2);
+   convertToRoman(3);
+   convertToRoman(4);
+   convertToRoman(5);
+   convertToRoman(9);
+   convertToRoman(12);
+   convertToRoman(16);
+   convertToRoman(29);
+   convertToRoman(44);
+   convertToRoman(45);
+   convertToRoman(68);
+   convertToRoman(83);
+   convertToRoman(97);
+   convertToRoman(99);
+   convertToRoman(400);
+   convertToRoman(500);
+   convertToRoman(501);
+   convertToRoman(649);
+   convertToRoman(798);
+   convertToRoman(891);
+   convertToRoman(1000);
+   convertToRoman(1004);
+   convertToRoman(1006);
+   convertToRoman(1023);
+   convertToRoman(2014);
+   convertToRoman(3999);
