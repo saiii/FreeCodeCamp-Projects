d1be114cff9e1806c827e05815e4ff19540b6ad9
Chayanon Tonsai <maxjunerd@gmail.com>
2019-12-14 13:53:03 +0700

    Adjust the size of Quotes

---
+++ Front End Libraries/Random Quote Machine/build/precache-manifest.2965d45d0d1da5e74dfc5349c2b66993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32d351aa22ab6ec1285fad12c869e28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a483049ccaf553d43af3",
+    "url": "./static/css/main.d24e7cc9.chunk.css"
+  },
+  {
+    "revision": "4e600d43f44879cb4d36",
+    "url": "./static/js/2.715602e4.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "./static/js/2.715602e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a483049ccaf553d43af3",
+    "url": "./static/js/main.2db1e099.chunk.js"
+  },
+  {
+    "revision": "0bd4aeaaa05506c4a137",
+    "url": "./static/js/runtime-main.ea53f173.js"
+  }
+]);
