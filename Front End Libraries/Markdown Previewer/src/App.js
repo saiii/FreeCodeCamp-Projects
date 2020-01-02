941b98f1b719367f26cab3148b403671d3dd702e
Chayanon Tonsai <maxjunerd@gmail.com>
2020-01-02 17:47:19 +0700

    Remove default

--- Front End Libraries/Markdown Previewer/src/App.js
+++ Front End Libraries/Markdown Previewer/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Editor from './containers/editor';
+import Preview from './components/preview';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
