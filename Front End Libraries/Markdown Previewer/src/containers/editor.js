3e3756400122aa1569999ab108780118ffbe0e0a
Chayanon Tonsai <maxjunerd@gmail.com>
2020-01-03 12:07:39 +0700

    Change how to render Editor

--- Front End Libraries/Markdown Previewer/src/containers/editor.js
+++ Front End Libraries/Markdown Previewer/src/containers/editor.js
@@ -2,14 +2,69 @@ import React, { Component } from 'react';
 import './editor.css';
 
 class Editor extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            markdown: placeholder
+        }
+    }
+
     render() {
         return (
             <div className='Editor'>
                 <div>Editor</div>
-                <textarea />
+                <textarea id='input' value={this.state.markdown}/>
             </div>
         );
     };
 };
+
+const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
 
 export default Editor;
