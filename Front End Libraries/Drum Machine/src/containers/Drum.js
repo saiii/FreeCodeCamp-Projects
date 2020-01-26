d0dd1ba4a6aae84457213abb54548f71df283cfb
Chayanon Tonsai <maxjunerd@gmail.com>
2020-01-26 16:44:51 +0700

    Add mode 2 sounds

--- Front End Libraries/Drum Machine/src/containers/Drum.js
+++ Front End Libraries/Drum Machine/src/containers/Drum.js
@@ -12,15 +12,15 @@ import classes from './Drum.module.css';
 class Drum extends Component {
     state = {
         keys: [
-            ['Q', 'Chord 1', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'],
-            ['W', 'Chord 2', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'],
-            ['E', 'Chord 3', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'],
-            ['A', 'Shaker', 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'],
-            ['S', 'Open HH', 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'],
-            ['D', 'Closed HH', 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'],
-            ['Z', 'Punchy Kick', 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'],
-            ['X', 'Side Stick', 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'],
-            ['C', 'Snare', 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3']
+            ['Q', 'Chord 1', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3', 'Heater 1', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'],
+            ['W', 'Chord 2', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3', 'Heater 2', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'],
+            ['E', 'Chord 3', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3', 'Heater 3', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'],
+            ['A', 'Shaker', 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3', 'Heater 4', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'],
+            ['S', 'Open HH', 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3', 'Clap', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'],
+            ['D', 'Closed HH', 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3', 'Open HH', 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'],
+            ['Z', 'Punchy Kick', 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3', "Kick n'Hat", 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'],
+            ['X', 'Side Stick', 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3', 'Kick', 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'],
+            ['C', 'Snare', 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3', 'Closed HH', 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3']
         ], 
         whichKey: null,
         volume: 1,
