1411b6c13adac79530f3337f51f09629e63640e1
Chayanon Tonsai <maxjunerd@gmail.com>
2020-02-14 00:42:31 +0700

    Add Calculator and Screen

---
+++ Front End Libraries/Calculator/src/components/screen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './screen.module.css';
+
+const Screen = props => {
+    return (
+        <div className={classes.Screen}>
+            <p>0</p>
+        </div>
+    )
+};
+
+export default Screen;
---
+++ Front End Libraries/Calculator/src/containers/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Screen from '../components/screen';
+
+const Calculator = props => {
+
+    return (
+        <div>
+            <Screen />
+        </div>
+    )
+
+};
+
+export default Calculator;
