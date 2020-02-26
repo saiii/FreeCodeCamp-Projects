bb371e4b97d74ab9b7145ae9d5d101436d824526
Chayanon Tonsai <maxjunerd@gmail.com>
2020-02-26 22:44:38 +0700

    Add background color

--- Front End Libraries/Pomodoro Clock/src/App.js
+++ Front End Libraries/Pomodoro Clock/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Clock from './containers/clock';
+import Clock from './containers/Clock';
 
 import './App.css';
 
--- Front End Libraries/Pomodoro Clock/src/containers/Clock.js
+++ Front End Libraries/Pomodoro Clock/src/containers/Clock.js
@@ -3,7 +3,11 @@ import React from 'react';
 import classes from './Clock.module.css';
 
 const Clock = () => {
-    
+    return (
+        <div className={classes.Clock}>
+
+        </div>
+    )
 };
 
 export default Clock;
