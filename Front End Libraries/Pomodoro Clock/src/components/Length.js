18de6a5222540efad5a215c38f97b2258a0096d0
Chayanon Tonsai <maxjunerd@gmail.com>
2020-02-26 23:00:20 +0700

    Add Length.js and Length.module.css

---
+++ Front End Libraries/Pomodoro Clock/src/components/Length.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+const Length = props => {
+    return (
+        <Fragment>
+            <p>{props.lengthType}</p>
+            
+        </Fragment>
+    )
+};
+
+export default Length;
