fd0afa36663e85dad966877a7f30fb77865e0809
Chayanon Tonsai <maxjunerd@gmail.com>
2020-02-26 22:53:24 +0700

    Add state

--- Front End Libraries/Pomodoro Clock/src/containers/Clock.js
+++ Front End Libraries/Pomodoro Clock/src/containers/Clock.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import classes from './Clock.module.css';
 
 const Clock = () => {
+    const [sessionLength, setSessionLength] = useState(25);
+    const [breakLength, setBreakLength] = useState(5);
+    const [running, setRunning] = useState(false);
+
     return (
         <div className={classes.Clock}>
             <h1>Pomodoro Clock</h1>
-            
+
         </div>
     )
 };
