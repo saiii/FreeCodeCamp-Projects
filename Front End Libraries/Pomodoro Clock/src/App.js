a74c6418f54e2ffe5cb6e184f38b35cf2faf6588
Chayanon Tonsai <maxjunerd@gmail.com>
2020-02-26 22:38:25 +0700

    Add Clock.js and Clock.module.css

--- Front End Libraries/Pomodoro Clock/src/App.js
+++ Front End Libraries/Pomodoro Clock/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import Clock from './containers/clock';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Clock />
   );
 }
 
---
+++ Front End Libraries/Pomodoro Clock/src/containers/Clock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Clock.module.css';
+
+const Clock = () => {
+    
+};
+
+export default Clock;
